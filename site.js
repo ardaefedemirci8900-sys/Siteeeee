cee2aea6c78c133dc0be8911b073015070281102
LyReachx <ardaefedemirci8900@gmail.com>
2026-03-25 11:25:28 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,9 @@
+const urunler = [
+    {
+        isim: "Marcas Royal Sneaker", // Ayakkabının adını buraya yaz
+        aciklama: "Günlük kullanım için elit ve rahat tasarım.", // Kısa reklam yazısı
+        fiyat: "2.499 TL", // Fiyatı
+        resimUrl: "https://..."
+    },
+    // Diğer ürünler...
+];
